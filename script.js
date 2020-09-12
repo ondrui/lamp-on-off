e49b7e8d60b65d5bcccb261930f7796c641077ea
ondrui <ondrui@yandex.ru>
2020-09-12 19:51:38 +0300

    Подшаманил

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 "use strict"
-// let lamp = document.querySelector(".lampBody");
-// console.log(lamp);
-// let lampSrc = lamp.querySelector("img");
-// console.log(lampSrc);
-// lampSrc.setAttribute("src", "img/lampOn.jpg");
 document.querySelector(".on").onclick = function() {
-  document.querySelector(".lampBody").querySelector("img").setAttribute("src", "img/lampOn.jpg");
-}
+  document.querySelector("img").setAttribute("src", "img/lampOn.jpg");
+};
 
 document.querySelector(".off").onclick = function() {
-  document.querySelector(".lampBody").querySelector("img").setAttribute("src", "img/lampOff.jpg");
-}
+  document.querySelector("img").setAttribute("src", "img/lampOff.jpg");
+};
 
 
